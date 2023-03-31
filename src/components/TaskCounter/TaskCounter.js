75cab3861b0a46eb037fd94be60e794e55a62ddd
Sergey Opanasenko <svopanasenko22@gmail.com>
2023-03-31 13:06:56 +0200

    y

--- src/components/TaskCounter/TaskCounter.js
+++ src/components/TaskCounter/TaskCounter.js
@@ -7,22 +7,21 @@ export const TaskCounter = () => {
   console.log(tasks);
 
   // На базе состояния Redux получаем производные данные
-  // const count = tasks.reduce((acc, task) => {
-  //   if (task.completed) {
-  //     acc.completed += 1;
-  //   } else {
-  //     acc.active += 1;
-  //   }
-  //   return acc;
-  // },
-  //   { active: 0, completed: 0 }
-  // );
+  const count = tasks.reduce((acc, task) => {
+    if (task.completed) {
+      acc.completed += 1;
+    } else {
+      acc.active += 1;
+    }
+    return acc;
+  },
+    { active: 0, completed: 0 }
+  );
 
   return (
     <div>
-      <p>Active:</p>
-      {/* <p>Active: {count.active}</p>
-      <p>Completed: {count.completed}</p> */}
+      <p>Active: {count.active}</p>
+      <p>Completed: {count.completed}</p>
     </div>
   );
 };
--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,7 @@ import { createStore } from 'redux';
 // Начальное значение состояния Redux для корневого редюсера,
 // если не передать параметр preloadedState.
 const initialState = {
-  task: [
+  tasks: [
     { id: 0, text: 'Learn HTML and CSS', completed: true },
     { id: 1, text: 'Get good at JavaScript', completed: true },
     { id: 2, text: 'Master React', completed: false },
