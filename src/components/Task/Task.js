ae008938c8bff67dce70ae205eee4eea076f4f7b
Sergey Opanasenko <svopanasenko22@gmail.com>
2023-03-31 12:16:29 +0200

    q

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -2,18 +2,20 @@ import { MdClose } from 'react-icons/md';
 import css from './Task.module.css';
 import { useDispatch } from 'react-redux';
 // Импортируем генератор экшена
-import { deleteTask } from '../../redux/actions';
+import { deleteTask, toggleCompleted } from '../../redux/actions';
 
 export const Task = ({ task }) => {
   // Получаем ссылку на функцию отправки экшенов
   const dispatch = useDispatch();
 
+  const handleDelete = () => dispatch(deleteTask(task.Id));
   // Вызываем генератор экшена и передаём идентификатор задачи
   // Отправляем результат - экшен удаления задачи
-  const handleDelete = () => dispatch(deleteTask(task.Id));
+  const handleToggle = () => dispatch(toggleCompleted(task.id));
+  
   return (
     <div>
-      <input type="checkbox"/>
+      <input type="checkbox" onChange={handleToggle} checked={task.completed} />
       <p>{task.text}</p>
       <button type="button" onClick={handleDelete}>
         Delete
--- src/components/TaskCounter/TaskCounter.js
+++ src/components/TaskCounter/TaskCounter.js
@@ -4,23 +4,25 @@ import { getTask } from 'redux/selectors';
 export const TaskCounter = () => {
   // Получаем массив задач из состояния Redux
   const tasks = useSelector(getTask);
+  console.log(tasks);
 
   // На базе состояния Redux получаем производные данные
-  const count = tasks.reduce((acc, task) => {
-    if (task.completed) {
-      acc.completed += 1;
-    } else {
-      acc.active += 1;
-    }
-    return acc;
-  },
-    { active: 0, completed: 0 }
-  );
+  // const count = tasks.reduce((acc, task) => {
+  //   if (task.completed) {
+  //     acc.completed += 1;
+  //   } else {
+  //     acc.active += 1;
+  //   }
+  //   return acc;
+  // },
+  //   { active: 0, completed: 0 }
+  // );
 
   return (
     <div>
-      <p>Active: {count.active}</p>
-      <p>Completed: {count.completed}</p>
+      <p>Active:</p>
+      {/* <p>Active: {count.active}</p>
+      <p>Completed: {count.completed}</p> */}
     </div>
   );
 };
