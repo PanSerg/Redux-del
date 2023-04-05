7acb2dc93daf45a8d7425b96b21f81693046725e
Sergey Opanasenko <svopanasenko22@gmail.com>
2023-04-05 11:03:08 +0200

    r

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { tasksReducer, filtersReducer } from './reducer';
+import { tasksReducer, filtersReducer } from './reduser';
 
 export const store = configureStore({
   reducer: {
